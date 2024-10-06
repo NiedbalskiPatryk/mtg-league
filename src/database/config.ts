4e16a5fcfdaf7a69feddcdf0386a84862457fec6
NiedbalskiPatryk <niedbalski.patryk@gmail.com>
2024-10-06 22:54:20 +0200

    DB setup done

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,8 +1,13 @@
-import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { PostgreSqlDriver, defineConfig } from '@mikro-orm/postgresql';
 
-export const mikroOrmConfig = {
+export const mikroOrmConfig: MikroOrmModule = defineConfig({
   entities: ['./dist/core/entities'],
   entitiesTs: ['./src/core/entities'],
-  dbName: 'mtg-league.db',
+  dbName: process.env.DB_NAME,
   driver: PostgreSqlDriver,
-};
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
